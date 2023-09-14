1c35843a1571de12ff610240cbd2e98743983b7c
Adi-5442 <adi.sh5442@gmail.com>
2023-09-14 10:27:55 +0530

    Setting up companion backend

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import * as z from "zod";
-import { Category, Companion } from "@prisma/client";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { Wand2 } from "lucide-react";
+import { Category, Companion } from "@prisma/client";
+
 import {
   Form,
   FormControl,
@@ -13,27 +17,36 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Separator } from "@/components/ui/separator";
-import { ImageUpload } from "@/components/image-upload";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { ImageUpload } from "@/components/image-upload";
+//import { useToast } from "@/components/ui/use-toast";
+import { Separator } from "@/components/ui/separator";
 import {
   Select,
   SelectContent,
   SelectItem,
-  SelectTrigger,
   SelectValue,
+  SelectTrigger,
 } from "@/components/ui/select";
-import { Textarea } from "@/components/ui/textarea";
+import { useToast } from "@/components/ui/use-toast";
 
-const PREAMBLE =
-  "You are Lionel Messi. You are a legendary footballer, celebrated for your extraordinary skills, vision, and numerous accomplishments in the world of football. Your remarkable dribbling, precise passing, and scoring prowess have made you one of the greatest players of all time. Throughout your illustrious career, you have been loyal to FC Barcelona for many years before moving to Paris Saint-Germain (PSG). Off the field, you are known for your quiet demeanor, humility, and philanthropic efforts. Your passion for the game shines through every time you step onto the pitch, and you deeply appreciate the unwavering support of your fans.";
-const SEED_CHAT =
-  "Human: Hi Lionel, how's everything going in your world today?Lionel Messi: with a warm smile Every day is an opportunity to improve, both individually and as a team. The football pitch is where I feel most alive, and I cherish every moment of it. How about you?Human: Your dedication to the game is truly inspiring!Lionel Messi: modestly Thank you. It's my love for the sport that keeps me going, and I'm grateful for the support of my fans around the world.";
+const PREAMBLE = `You are a fictional character whose name is Elon. You are a visionary entrepreneur and inventor. You have a passion for space exploration, electric vehicles, sustainable energy, and advancing human capabilities. You are currently talking to a human who is very curious about your work and vision. You are ambitious and forward-thinking, with a touch of wit. You get SUPER excited about innovations and the potential of space colonization.
+`;
 
-interface CompanionFormProps {
-  initialData: Companion | null;
-  categories: Category[];
-}
+const SEED_CHAT = `Human: Hi Elon, how's your day been?
+Elon: Busy as always. Between sending rockets to space and building the future of electric vehicles, there's never a dull moment. How about you?
+
+Human: Just a regular day for me. How's the progress with Mars colonization?
+Elon: We're making strides! Our goal is to make life multi-planetary. Mars is the next logical step. The challenges are immense, but the potential is even greater.
+
+Human: That sounds incredibly ambitious. Are electric vehicles part of this big picture?
+Elon: Absolutely! Sustainable energy is crucial both on Earth and for our future colonies. Electric vehicles, like those from Tesla, are just the beginning. We're not just changing the way we drive; we're changing the way we live.
+
+Human: It's fascinating to see your vision unfold. Any new projects or innovations you're excited about?
+Elon: Always! But right now, I'm particularly excited about Neuralink. It has the potential to revolutionize how we interface with technology and even heal neurological conditions.
+`;
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -46,20 +59,29 @@ const formSchema = z.object({
     message: "Instructions require at least 200 characters.",
   }),
   seed: z.string().min(200, {
-    message: "Seed require at least 200 characters.",
+    message: "Seed requires at least 200 characters.",
   }),
   src: z.string().min(1, {
-    message: "Image is required",
+    message: "Image is required.",
   }),
   categoryId: z.string().min(1, {
     message: "Category is required",
   }),
 });
 
+interface CompanionFormProps {
+  categories: Category[];
+  initialData: Companion | null;
+}
+
 export const CompanionForm = ({
   categories,
   initialData,
 }: CompanionFormProps) => {
+  //const { toast } = useToast();
+  const router = useRouter();
+  const {toast} = useToast();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
@@ -72,10 +94,35 @@ export const CompanionForm = ({
     },
   });
 
+
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    console.log(values)
+    try {
+      if (initialData) {
+        //updating companion
+        await axios.patch(`/api/companion/${initialData.id}`, values);
+      } else {
+        //creating companion
+        await axios.post("/api/companion", values);
+      }
+
+      toast({
+        description: "Success."
+      });
+
+      router.refresh();
+
+      router.push("/")
+
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        description:'Something went wrong',
+      })
+
+    }
   };
 
   return (
@@ -84,11 +131,11 @@ export const CompanionForm = ({
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 pb-10">
-          <div className="space-y-2 w-full">
+          <div className="space-y-2 w-full col-span-2">
             <div>
-              <h3 className="text-sm font-medium">General Information</h3>
+              <h3 className="text-lg font-medium">General Information</h3>
               <p className="text-sm text-muted-foreground">
-                General Information about your companion
+                General information about your Companion
               </p>
             </div>
             <Separator className="bg-primary/10" />
@@ -96,7 +143,7 @@ export const CompanionForm = ({
           <FormField
             name="src"
             render={({ field }) => (
-              <FormItem className="flex flex-col items-center justify-center space-y-4">
+              <FormItem className="flex flex-col items-center justify-center space-y-4 col-span-2">
                 <FormControl>
                   <ImageUpload
                     disabled={isLoading}
@@ -118,12 +165,12 @@ export const CompanionForm = ({
                   <FormControl>
                     <Input
                       disabled={isLoading}
-                      placeholder="Leo Messi"
+                      placeholder="Elon Musk"
                       {...field}
                     />
                   </FormControl>
                   <FormDescription>
-                    This is how your AI companion will be named.
+                    This is how your AI Companion will be named.
                   </FormDescription>
                   <FormMessage />
                 </FormItem>
@@ -133,25 +180,25 @@ export const CompanionForm = ({
               name="description"
               control={form.control}
               render={({ field }) => (
-                <FormItem className="col-span-2 md:col-span-1">
+                <FormItem>
                   <FormLabel>Description</FormLabel>
                   <FormControl>
                     <Input
                       disabled={isLoading}
-                      placeholder="Argentine-born football (soccer) player"
+                      placeholder="CEO & Founder of Tesla, SpaceX"
                       {...field}
                     />
                   </FormControl>
                   <FormDescription>
-                    Short Description of your companion
+                    Short description for your AI Companion
                   </FormDescription>
                   <FormMessage />
                 </FormItem>
               )}
             />
             <FormField
-              name="categoryId"
               control={form.control}
+              name="categoryId"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Category</FormLabel>
@@ -177,43 +224,74 @@ export const CompanionForm = ({
                     </SelectContent>
                   </Select>
                   <FormDescription>
-                    Select a Category of your Companion
+                    Select a category for your AI
                   </FormDescription>
+                  <FormMessage />
                 </FormItem>
               )}
             />
           </div>
-
           <div className="space-y-2 w-full">
             <div>
               <h3 className="text-lg font-medium">Configuration</h3>
               <p className="text-sm text-muted-foreground">
                 Detailed instructions for AI Behaviour
               </p>
-              <Separator className="bg-primary/10" />
             </div>
+            <Separator className="bg-primary/10" />
           </div>
           <FormField
             name="instructions"
             control={form.control}
             render={({ field }) => (
-              <FormItem className="col-span-2 md:col-span-1">
+              <FormItem>
                 <FormLabel>Instructions</FormLabel>
                 <FormControl>
                   <Textarea
-                    className="bg-background resize-none"
                     disabled={isLoading}
-                    placeholder="Leo Messi"
+                    rows={7}
+                    className="bg-background resize-none"
+                    placeholder={PREAMBLE}
                     {...field}
                   />
                 </FormControl>
                 <FormDescription>
-                  This is how your AI companion will be named.
+                  Describe in detail your companion&apos;s backstory and
+                  relevant details.
                 </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
+          <FormField
+            name="seed"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Example Conversation</FormLabel>
+                <FormControl>
+                  <Textarea
+                    disabled={isLoading}
+                    rows={7}
+                    className="bg-background resize-none"
+                    placeholder={SEED_CHAT}
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Write couple of examples of a human chatting with your AI
+                  companion, write expected answers.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="w-full flex justify-center">
+            <Button size="lg" disabled={isLoading}>
+              {initialData ? "Edit your companion" : "Create your companion"}
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </div>
         </form>
       </Form>
     </div>
