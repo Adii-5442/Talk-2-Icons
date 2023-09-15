a9376f582b4030f0da10beca07338491e87dae34
Adi-5442 <adi.sh5442@gmail.com>
2023-09-15 21:41:21 +0530

    WIP - chat screen

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Companion, Message } from "@prisma/client";
+import { ChatMessage } from "./chat-message";
 
 interface ChatMessagesProps {
     isLoading: boolean;
@@ -11,8 +12,12 @@ interface ChatMessagesProps {
 export const ChatMessages = ({ isLoading, companion, messages = [], }: ChatMessagesProps) => {
 
     return (
-        <div>
-            Chats
+        <div className="flex-1 overflow-y-auto pr-4">
+            <ChatMessage
+                src={companion.src}
+                role="system"
+                content={`Hello I am ${companion.name} , ${companion.description}`}
+            />
         </div>
     )
 }
