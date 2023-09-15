956acd196149716c64438c53b949f9307232ab6b
Adi-5442 <adi.sh5442@gmail.com>
2023-09-15 22:03:33 +0530

    CHat UI implemented

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import {useCompletion} from "ai/react"
 import { ChatForm } from "@/components/chat-form";
 import { ChatMessages } from "@/components/chat-messages";
+import { ChatMessageProps } from "@/components/chat-message";
 interface ChatClientProps {
     companion: Companion & {
         messages: Message[];
@@ -17,13 +18,13 @@ interface ChatClientProps {
 
 export const ChatClient = ({ companion }: ChatClientProps) => {
     const router = useRouter();
-    const [messages, setMessages] = useState<any[]>(companion.messages);
+    const [messages, setMessages] = useState<ChatMessageProps[]>(companion.messages);
 
 
     const { input, isLoading, handleInputChange, handleSubmit, setInput } = useCompletion({
         api: `/api/chat/${companion.id}`,
         onFinish(prompt, completion) {
-            const systemMessage = {
+            const systemMessage :ChatMessageProps = {
                 role: "system",
                 content: completion,
             };
@@ -35,10 +36,9 @@ export const ChatClient = ({ companion }: ChatClientProps) => {
     })
 
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-        const userMessage = {
-            role: "user",
-            content: input,
-
+        const userMessage: ChatMessageProps = {
+          role: "user",
+          content: input,
         };
         setMessages((current) => [...current, userMessage]);
 
--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -1,23 +1,63 @@
-"use client"
+"use client";
 
 import { Companion, Message } from "@prisma/client";
-import { ChatMessage } from "./chat-message";
+import { ChatMessage, ChatMessageProps } from "./chat-message";
+import { ElementRef, useEffect, useRef, useState } from "react";
 
 interface ChatMessagesProps {
-    isLoading: boolean;
-    companion: Companion
-    messages: any[];
+  isLoading: boolean;
+  companion: Companion;
+  messages: ChatMessageProps[];
 }
 
-export const ChatMessages = ({ isLoading, companion, messages = [], }: ChatMessagesProps) => {
+export const ChatMessages = ({
+  isLoading,
+  companion,
+  messages = [],
+}: ChatMessagesProps) => {
+    const scrollRef = useRef<ElementRef<"div">>(null);
+  const [fakeLoading, setFakeLoading] = useState(
+    messages.length === 0 ? true : false
+  );
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFakeLoading(false);
+    }, 1000);
 
-    return (
-        <div className="flex-1 overflow-y-auto pr-4">
-            <ChatMessage
-                src={companion.src}
-                role="system"
-                content={`Hello I am ${companion.name} , ${companion.description}`}
-            />
-        </div>
-    )
-}
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+    useEffect(() => {
+        scrollRef?.current?.scrollIntoView({ behavior: "smooth" });
+
+    }, [messages.length])
+
+
+  return (
+    <div className="flex-1 overflow-y-auto pr-4">
+      <ChatMessage
+        isLoading={fakeLoading}
+        src={companion.src}
+        role="system"
+        content={`Hello I am ${companion.name} , ${companion.description}`}
+          />
+          {messages.map((message) => (
+              <ChatMessage
+                  key={message.content}
+                  role={message.role}
+                  content={message.content}
+                  src={message.src}
+              />
+          ))}
+          {isLoading && (
+              <ChatMessage
+                  role="system"
+                  src={companion.src}
+                  isLoading
+              />
+          )}
+          <div ref={scrollRef } />
+    </div>
+  );
+};
